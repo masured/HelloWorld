0e16d80605f72e7ec8ba6e4d317dd56862134546
dmasure <denis_fr@web.de>
2018-06-05 23:59:00 +0200

    update of the order in the hello sentence

--- src/fr/dmas/ocrexamples/Main.java
+++ src/fr/dmas/ocrexamples/Main.java
@@ -1,9 +1,11 @@
 package fr.dmas.ocrexamples;
 
+import fr.dmas.ocrexamples.utils.CurrentTime;
+import fr.dmas.ocrexamples.utils.ScanName;
+
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello OpenClassrooms");
-
+        String name = ScanName.getName();
+        System.out.println("Hi "+name+", it is : "+CurrentTime.getTime()+", Welcome to openClassRooms !");
     }
 }
