89081c23e05af9a828de7731395f6e6c768e0247
dmasure <denis_fr@web.de>
2018-06-05 23:55:17 +0200

    update the question about the name : politer

--- src/fr/dmas/ocrexamples/utils/ScanName.java
+++ src/fr/dmas/ocrexamples/utils/ScanName.java
@@ -1,4 +1,11 @@
 package fr.dmas.ocrexamples.utils;
 
+import java.util.Scanner;
+
 public class ScanName {
+    public static String getName() {
+        System.out.print("What is your name please? ");
+        Scanner name = new Scanner(System.in);
+        return name.nextLine();
+    }
 }
