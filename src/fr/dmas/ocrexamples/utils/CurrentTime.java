1eccae7a6fa4e9b3e0bbe8ad7316101cbb358ebc
dmasure <denis_fr@web.de>
2018-06-05 23:53:12 +0200

    delete the seconds information

--- src/fr/dmas/ocrexamples/utils/CurrentTime.java
+++ src/fr/dmas/ocrexamples/utils/CurrentTime.java
@@ -1,4 +1,12 @@
 package fr.dmas.ocrexamples.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class CurrentTime {
+    public static String getTime() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        return sdf.format(cal.getTime());
+    }
 }
